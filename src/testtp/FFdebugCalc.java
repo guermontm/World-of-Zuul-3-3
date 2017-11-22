56c8f93f3fed9634f9de08bb19817673be41b2a3
pandreo <andreopauline@gmail.com>
2017-11-22 16:29:05 +0100

    "commit par pauline"

---
+++ src/testtp/FFdebugCalc.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testtp;
+
+/**
+ *
+ * @author Pauline
+ */
+public class FFdebugCalc {
+
+    /**
+     * modif modif 
+     * @param array
+     * @return 
+     */
+    public static int findMax(int[] array) {
+        int max = array[0];
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
+                // commentaire pour le commit de Pauline
+            }
+        }
+        return max;
+    }
+
+    public static int findMin(int[] array) {
+        int min = array[0];
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < min) {
+                min = array[i];
+            }
+        }
+        return min;
+    }
+}
