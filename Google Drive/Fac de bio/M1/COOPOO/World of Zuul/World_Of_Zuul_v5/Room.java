45ea5c66ede6f75bf45f205826c56a3a5569a352
Adele.R <adelerichard@free.fr>
2017-11-23 09:49:21 +0100

    "premier commit du World of Zuul v5"

---
+++ Google Drive/Fac de bio/M1/COOPOO/World of Zuul/World_Of_Zuul_v5/Room.java
@@ -0,0 +1,102 @@
+import java.util.HashMap;
+/**
+ * The Room class. Corresponds to the room in which the player is.
+ * Changes each time the player room according to his direction.
+ * All the rooms are linked together.
+ * @author (Morgane GUERMONT, Adèle RICHARD, Nolan LACROIX, Pauline ANDREO, Clément RISSO)
+ * @version (0.1)
+ */
+public class Room
+{
+    // instance variables for the Room class.
+    // Name of the room
+    protected String roomName;
+    // Description of the room that is displayed when the player enters
+    protected String roomDescription;
+    // List of items in the room
+    protected Item roomItem;
+
+    // // The four exits of the room.
+    // private Room northExit;
+    // private Room southExit;
+    // private Room westExit;
+    // private Room eastExit;
+    
+    // Hasmap containing all the rooms and their direction. Used to position the player.
+    protected HashMap<String,Room> mapRoom;
+    
+    /**
+     * Constructor for objects of class Room
+     */
+    public Room()
+    {
+        // initialise instance variables
+        roomName = "Default Room";
+        roomDescription = "You are not supposed to see this. If you see this, shit went wrong at some point. Please report.";
+        //roomItem = new Item("Testitem", 1, false);
+        mapRoom = new HashMap<String,Room>();
+    }
+
+    /**
+     * Constructor for objects of class Room
+     */
+    public Room(String name, String description)
+    {
+        roomName = name;
+        roomDescription = description;
+        //roomItem = new Item("Testitem", 1, false);
+        mapRoom = new HashMap<String,Room>();
+    }
+
+    /**
+    * Getter for the name of the Room.
+    * @return roomName name of the room.
+    */
+    public String getName()
+    {
+        return roomName;
+    }
+
+    /**
+    * Getter for the description of the room.
+    * @return roomDescription description of the room.
+    */
+    public String getDescription()
+    {
+        return roomDescription;
+    }
+
+    // /**
+    // * Getter for all the exits in the room.
+    // * @return a String with all the exits concatenated.
+    // */
+    // public String getAllExits()
+    // {
+    //     return ("\nNorth: " + northExit.roomName + "\nSouth: " + southExit.roomName + "\nEast:" + eastExit.roomName + "\nWest:" + westExit.roomName + "\n");
+    // }
+
+    /**
+    * Setter for the four exits of the room.
+    * @param north room in the north of this one.
+    * @param south room in the south of this one.
+    * @param east room in the east of this one.
+    * @param west room in the west  of this one.
+    */
+    public void setExits(Room north, Room east, Room south, Room west)
+    {
+        mapRoom.put("north",north);
+        mapRoom.put("east",east);
+        mapRoom.put("south",south);
+        mapRoom.put("west",west);
+    }
+
+    /**
+    * Accessor for the different exits of a room.
+    * @return a hashmap containing the exits.
+    */
+    public HashMap<String,Room> getExits()
+    {
+        return(mapRoom);
+    }
+
+}
