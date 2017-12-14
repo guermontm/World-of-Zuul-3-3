c78861ca340ef0d44c69f58db64bfa92e9fe882f
NolanLacroix <33682165+NolanLacroix@users.noreply.github.com>
2017-12-14 22:31:30 +0100

    Update Player.java
    add comm

--- src/code/Player.java
+++ src/code/Player.java
@@ -19,7 +19,7 @@ abstract class Player
     private int money; // money of the player (>0)
     private boolean crowbar; // if the player has the crowbar or not. Starts at false
     private boolean keyAnnie; //a key for the office of Mrs Geniet, starts at false.
-    private boolean keyPG;
+    private boolean keyPG; //a key for the office of Mr PG, starts at false
 
     /**
      * Constructor for objects of class Player
@@ -33,114 +33,212 @@ abstract class Player
         money = 4;
     }
 
+    /**
+     * getNamePlayer Method, allowing to return the name of the player
+     * @return the String name, being the name of the player
+     */
     public String getNamePlayer ()
     {
       return name;   
     }
-        public void setNamePlayer (String newName)
+    
+    /**
+     * setNamePlayer Method, allowing to set the name of the player with a new name
+     * @param the String newName, being the new name of the player
+     */
+    public void setNamePlayer (String newName)
     {
         name = newName;
     }
+    
+    /**
+     * getStrStat Method, allowing to return the strength value of the player
+     * @return the integer strStat, being the strength of the player
+     */
     public int getStrStat ()
     {
         return strStat;
     }
     
+    /**
+     * getSpeStat Method, allowing to return the speech value of the player
+     * @return the integer speStat, being the speech of the player
+     */
     public int getSpeStat ()
     {
         return speStat;
     }
     
+    /**
+     * getIntStat Method, allowing to return the intelligence value of the player
+     * @return the integer intStat, being the intelligence of the player
+     */
     public int getIntStat ()
     {
         return intStat;
     }
     
+    /**
+     * getStaStat Method, allowing to return the stamina value of the player
+     * @return the integer staStat, being the stamina value of the player
+     */
     public int getStaStat ()
     {
         return staStat;
     }
     
+    /**
+     * getStressStat Method, allowing to return the stress value of the player
+     * @return the integer stressStat, being the stress of the player
+     */
     public int getStressStat ()
     {
         return stressStat;
     }
     
+    /**
+     * getMoney Method, allowing to return the money value of the player
+     * @return the integer money, being the money value of the player
+     */
     public int getMoney ()
     {
         return money;
     }
     
+    /**
+     * getCrowbar Method, allowing to return the boolean value of the player.
+     * If true, the player is owning a crowbar.
+     * @return the boolean crowbar
+     */
     public boolean getCrowbar ()
     {
         return crowbar;
     }
     
+    /**
+     * getKeyAnnie Method, allowing to return the boolean value of the player.
+     * If true, the player is owning the key of the Annie's office.
+     * @return the boolean keyAnnie
+     */
     public boolean getKeyAnnie()
     {
         return keyAnnie;
     }
     
+    /**
+     * getKeyPG Method, allowing to return the boolean value of the player.
+     * If true, the player is owning the key of the PG's office.
+     * @return the boolean keyPG
+     */
     public boolean getKeyPG()
             {
                 return keyPG;
             }
     
+    /**
+     * setStr Method, allowing to set the strength of the player with a new value
+     * @param str integer, being the new value for the player stat.
+     */
     public void setStr (int str)
     {
         strStat = str;
     }
     
+    /**
+     * setSpe Method, allowing to set the speech of the player with a new value
+     * @param spe integer, being the new value for the player stat.
+     */
     public void setSpe (int spe)
     {
         speStat = spe;
     }
     
+    /**
+     * setInt Method, allowing to set the intelligence of the player with a new value
+     * @param attint integer, being the new value for the player stat.
+     */
     public void setInt (int attint)
     {
         intStat = attint;
     }
     
+    /**
+     * setSta Method, allowing to set the stamina of the player with a new value
+     * @param sta integer, being the new value for the player stat.
+     */
     public void setSta (int sta)
     {
         staStat = sta;
     }
     
+    /**
+     * setStress Method, allowing to set the stress of the player with a new value
+     * @param stress integer, being the new value for the player stat.
+     */
     public void setStress (int stress)
     {
         stressStat = stress;
     }
     
+    /**
+     * setMoney Method, allowing to set money of the player with a new value
+     * @param money integer, being the new value for the player stat.
+     */
     public void setMoney (int money)
     {
        this.money = money;
     }
     
+    /**
+     * setCrowbar Method, allowing to set the boolean crowbar of the player with a new value
+     * @param bool integer, being the new value for the player stat.
+     */
     public void setCrowbar (boolean bool)
     {
        crowbar = bool;
     }
     
+    /**
+     * setKeyAnnie Method, allowing to set the boolean keyAnnie of the player with a new value
+     * @param bool integer, being the new value for the player stat.
+     */
     public void setKeyAnnie(boolean bool)
     {
         keyAnnie = bool;
     }
     
+    /**
+     * setKeyPG Method, allowing to set the boolean keyPG of the player with a new value
+     * @param bool integer, being the new value for the player stat.
+     */
      public void setKeyPG(boolean bool)
     {
         keyPG = bool;
     }
     
+    /**
+     * addMoney Method, allowing to add a cash value to the current money of the player.
+     * @param cash integer, being the value added to the current money.
+     */
     public void addMoney (int cash)
     {
        money = money + cash;
     }
     
+    /**
+     * substractMoney Method, allowing to substract a cash value to the current money of the player.
+     * @param cash integer, being the value removed to the current money.
+     */
     public void substractMoney (int cash)
     {
        money = money - cash;
     }
     
+    /**
+     * addStat Method, allowing to add a value for a current whatStat stat.
+     * @param whatStat String, being the stat to increase ; value integer, being
+     * the value to add to the stat.
+     */
     public void addStat (String whatStat, int value)
     {
         switch(whatStat) {
@@ -167,6 +265,12 @@ abstract class Player
             }
     }
 
+    /**
+     * substractStat Method, allowing to substract a value for a current 
+     * whatStat stat.
+     * @param whatStat String, being the stat to decrease ; value integer, being
+     * the value to substract to the stat.
+     */
     public void substractStat (String whatStat, int value)
     {
         switch(whatStat) {
