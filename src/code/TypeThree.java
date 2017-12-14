f67a3e420c2273c848dd025b13cf932b4097f599
NolanLacroix <33682165+NolanLacroix@users.noreply.github.com>
2017-12-14 22:23:06 +0100

    Update TypeThree.java
    add comm

--- src/code/TypeThree.java
+++ src/code/TypeThree.java
@@ -3,18 +3,21 @@ package code;
 
 /**
  * This class inherits from player and represents a player of the third type
- * It means that the attributes are initialised with specific values
+ * It means that the attributes are initialised with specific values.
+ * This player is named Elie and owns a special power : "lucky". This power can be activated or disabled.
+ * If "lucky" is on, the player is more likely to find coins.
  *
  * @author (Group 5)
  * @version (09/11/17)
  */
 public class TypeThree extends Player
 {
-    boolean lucky; // if the bool is true, the player is lucky and find more coins
+    boolean lucky; // if the boolean is true, the player is lucky and find more coinss
     
     /**
      * Constructor for objects of class TypeThree
      * This method gives the specific values of the different attributes 
+     * The player has also a lucky boolean.
      */
     public TypeThree()
     {
@@ -27,6 +30,11 @@ public class TypeThree extends Player
         setStress(4);
     }
     
+    /**
+     * Method getLucky allowing to return the boolean lucky
+     *
+     * @return the boolean lucky
+     */
     public boolean getLucky () 
     {
         return lucky;   
