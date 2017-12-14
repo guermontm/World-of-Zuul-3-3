67de58c5e007496ad97115332769c7014a1910aa
guermomo <guermontm@gmail.com>
2017-12-14 18:29:06 +0100

    "petites modifs des phrases"

--- src/code/Sentences.java
+++ src/code/Sentences.java
@@ -150,14 +150,14 @@ public class Sentences
        sentenceAxel3="[Axel] If you get tired, you can buy food here."; //trick
        
        sentenceMartin1="[Martin] Hurry up ! We have to code, what are you waiting for?";
-       sentenceMartin2="[Martin] Thanks for my L3 diploma ;)";
+       sentenceMartin2="[Martin] Thanks for my L3 diploma mate";
        sentenceMartin3="[Martin] Je vois pas pourquoi je parlerais anglais dans ton jeu, je suis un PNJ libre";
        
-       sentenceNolan1="[Nolan] I have the toilet phobia now..";
+       sentenceNolan1="[Nolan] I have a toilet phobia now..";
        sentenceNolan2="[Nolan] I am hungry";
-       sentenceNolan3="[Nolan] Let's go to work, I want to go home";
+       sentenceNolan3="[Nolan] Please hurry up, I want to be free again!";
        
-       sentenceValentin1="[Valentin] I am loner, my life is over. The topic of COOPOO stares at me, I want to become nobody";
+       sentenceValentin1="[Valentin] I am so sad, my life is over. The topic of COOPOO stares at me, I want to end this.";
        sentenceValentin2="[Valentin] *leans out of the window* I want to finish it, My COOPOO mates are not here";
        
        sentenceGuardian1="[The Guardian] I am your father";
@@ -167,8 +167,8 @@ public class Sentences
        sentenceThomas1="[Thomas] Alright";
        sentenceThomas2="[Thomas] psss psss pssssss *does not speak loudly, it seems not possible to hear him*";
        
-       sentenceMorgane1="[Morgane] *Burp*";
-       sentenceMorgane2="[Morgane] It's not because you are in a game that you have to speak to everybody, bitc*";
+       sentenceMorgane1="[Morgane] Hellooooo! What are you doing here? I heard that Martin was looking or you!";
+       sentenceMorgane2="[Morgane] Go away please, I don't want to talk to you right now";
        
        sentenceMarie1="[Marie] Come with us to the escape game tonight !";
        sentenceMarie2="[Marie] Don't forget there are 9 other projects to finish after the COOPOO subject !";
@@ -180,9 +180,9 @@ public class Sentences
        sentenceClement2="[Clement] I do not speak to those who come from elsewhere than Aurillac.";
        
        
-       sentenceM21="Hey ! We are launching a new virtual company. Do you want to come with us?";
-       sentenceM22="You're performing the COOPOO project ?! Good luck, it was hard last year !";
-       sentenceM23="*Singing*";
+       sentenceM21="[M2 Students] Hey ! We are launching a new virtual company. Do you want to come with us?";
+       sentenceM22="[M2 Students] You're performing the COOPOO project ?! Good luck, it was hard last year !";
+       sentenceM23="[M2 Students] *Singing*";
        
        //add the simple sentences to the list
        listAxel.add(sentenceAxel1);
@@ -225,13 +225,13 @@ public class Sentences
        sentenceMainMrsGeniet2="[Mrs Geniet] Yes, it's about time that he thinks about that! Take the keys to my office, but don't forget to bring them back.";
        sentenceMainMrsGeniet3="[Mrs Geniet] You need to be more quiet, students are presenting here...";
        sentenceMainMrsGeniet4="[Mrs Geniet] Are you kinding me ?! You do it on purpose ?! you are fired from GPhy."; //game over
-       sentenceMainMrsGeniet5="[Mrs Geniet] Thanks to bringing my keys back.";
+       sentenceMainMrsGeniet5="[Mrs Geniet] Thanks for bringing my keys back.";
        sentenceMainMrsGeniet6="[Mrs Geniet] You, here !! You had to bring me the keys ! Give them to me."; //+1 stress
        
        sentenceMainAxel1="[Axel] Ah ! You are finally here ! Have you already done your COOPOO exercise?";
        sentenceMainAxel2="[Axel] Humm, you're going to have problems if you do not do it today";
        sentenceMainAxel3="[Axel] No, I'm waiting for my group to start working. They will arrive soon..";
-       sentenceMainAxel4="[Axel] I advise you to get the subject from PG !";
+       sentenceMainAxel4="[Axel] You should go get the subject from PG !";
        sentenceMainAxel5="[Axel] Yeah but I already have the subject ! If you want, go see PG in TD4.";
        sentenceMainAxel6="[Axel] He is in TD4, go ahead quickly !";
        sentenceMainAxel7="[Axel] Yes.. Take care. He's in TD4. Good luck !";
@@ -239,19 +239,19 @@ public class Sentences
        sentenceMainMartin1="[Martin] Hey, how are you?";
        sentenceMainMartin2="[Martin] Yes, but I can't. I need my diploma..";
        sentenceMainMartin3="[Martin] Nothing. I need my diploma, but I'm lazy...";
-       sentenceMainMartin4="[Martin] No, I am just lazy. I think Mrs Geniet has my diploma, but she is doing the oral presentations.";
-       sentenceMainMartin5="[Martin] I guess the diploma is inside the Mrs Geniet's office. But actually, she's doing the oral presentations.";
-       sentenceMainMartin6="[Martin] Nop, ahah ! Please, be friendly, help me. She is doing the oral presentations !";
+       sentenceMainMartin4="[Martin] No, I am just lazy. I think Mrs Geniet has my diploma, but she is watching oral presentations.";
+       sentenceMainMartin5="[Martin] I guess the diploma is inside Mrs Geniet's office. But she's currently doing the oral presentations.";
+       sentenceMainMartin6="[Martin] Nope, ahah ! Please, be friendly, help me. She is doing the oral presentations !";
        sentenceMainMartin7="[Martin] Thanks my friend !";
        sentenceMainMartin8="[Martin] Oh, yes, you're really nice ! Thanks !";
        
        sentenceMainNolan1="[Nolan] Hello? Help ! Someone there?";
-       sentenceMainNolan2="[Nolan] I heard you, do not pretend not to hear me ! I'll tear your eyes off if you do not help me !"; //+1 stress
-       sentenceMainNolan3="*The person breaks the door with a supremely powerful force, jumps on you, catchs you, then plunges your head into the toilet. You died drowned.*"; //GO
-       sentenceMainNolan4="[Nolan] Ahhh ! It's you? I recognized your voice. Please, help me ! It's been two hours since I am locked";
+       sentenceMainNolan2="[Nolan] I heard you, do not pretend you did not hear me ! I'll tear your eyes off if you do not help me !"; //+1 stress
+       sentenceMainNolan3="*The person breaks the door with a supremely powerful force, jumps on you, catches you, then plunges your head into the toilet. You died drowned.*"; //GO
+       sentenceMainNolan4="[Nolan] Ahhh ! It's you? I recognized your voice. Please, help me ! I have been locked for two hours";
        sentenceMainNolan5="[Nolan] The lock is blocked, I have tried everything..";
-       sentenceMainNolan6="[Nolan] No ! Don't do that. I'm going to look smart afterwards...";
-       sentenceMainNolan7="[Nolan] Yes, well, get me out of here, solve quickly that ! Faster !";
+       sentenceMainNolan6="[Nolan] No ! Don't do that. I'm going to look stupid afterwards...";
+       sentenceMainNolan7="[Nolan] Yes, well, get me out of here, be quick ! Faster !";
        sentenceMainNolan8="[Nolan] Bweeerk, no ! Ahah, hurry up to help me please !";
        
        sentenceMainGuardian1="[The Guardian] Hello. I can feel that you need help.";
@@ -262,9 +262,9 @@ public class Sentences
        sentenceMainGuardian6="[The Guardian] Take care. You have to be strong to use it.";
        
        sentenceMainPGTD1="[PG] Yes, hello?";
-       sentenceMainPGTD2="[PG] It's time to start, right? It is to return for 18h, don't forget it...";
-       sentenceMainPGTD3="[PG] Rude ! Start by saying hello !! "; // +1 stess
-       sentenceMainPGTD4="[PG] No, but what is this behavior? Imagine yourself in a professional environment... You are not worthy of being a Gphy. Go away";//Game over
+       sentenceMainPGTD2="[PG] It's time to start, right? You have to return it for 18h, don't forget it...";
+       sentenceMainPGTD3="[PG] That is not polite ! Start by saying hello !! "; // +1 stess
+       sentenceMainPGTD4="[PG] What is this behavior? Imagine yourself in a professional environment... You are not worthy of being a Gphy. Go away";//Game over
        sentenceMainPGTD5="[PG] Hum.. Ok, be careful next time...";
        sentenceMainPGTD6="[PG] Ah, because you're going to watch them and do nothing? Pff, go to work..."; // +1 stress
        sentenceMainPGTD7="[PG] Yes... Do not waste your time remaining. This is not very serious...";
@@ -272,16 +272,16 @@ public class Sentences
        sentenceMainPGEnd1="[PG] You ! Here ! What are you doing ?!";
        sentenceMainPGEnd2="[PG] How did you get this key?! My key.";
        sentenceMainPGEnd3="[PG] It seems to me that the door of my office was closed !!! How did you get my key?"; //+1 stress
-       sentenceMainPGEnd4="[PG] Ahah ! Idiot... you are fired."; // game over
+       sentenceMainPGEnd4="[PG] Ahah ! You fool... you are fired."; // game over
        sentenceMainPGEnd5="[PG] Hum... Maybe I forgot to close the door when I left. Why did you want to see me?";
-       sentenceMainPGEnd6="[PG] Hum, I'm going to ask him if what you're saying is right. Stay here, wait me.";
-       sentenceMainPGEnd7="[PG] *You are lefting the B2 and running in the rain. Under the guilt, you collapse and died.";
-       sentenceMainPGEnd8="[PG] I'm back. You lied to me. You opened my office with my key. You are fired !!";
+       sentenceMainPGEnd6="[PG] Hum, I'm going to ask him if this is true. Stay here, wait for me.";
+       sentenceMainPGEnd7="[PG] *You are leaving the B2 and running in the rain. Under the guilt, you collapse and die."; //game over
+       sentenceMainPGEnd8="[PG] I'm back. You lied to me. You opened my office with my key. You are fired !!"; //game over
        sentenceMainPGEnd9="[PG] Give me the copy, please"; 
        sentenceMainPGEnd10="[PG] Are you serious?! Give me this copy !!"; //+1 stress
        sentenceMainPGEnd11="[PG] So, What are you waiting for !?";
        sentenceMainPGEnd12="[PG] Hum, interesting. Where are your group members?";
-       sentenceMainPGEnd13="*Unluckily, the door had been closed. So you will have a very bad time...*"; //game over
+       sentenceMainPGEnd13="*Unluckily, the door was closed. So you will have a very bad time...*"; //game over
        sentenceMainPGEnd14="[PG] It's not very nice of them ...";
        sentenceMainPGEnd15="[PG] Okay. But they could have come with you.";
        sentenceMainPGEnd16="[PG] I see... Let's look at this copy. Hum, ok. Should an abstract class be instantiated?";
@@ -292,8 +292,8 @@ public class Sentences
        sentenceMainPGEnd21="[PG] No, yet it's easy !! And should an abstract class be redefined in the child classes?"; //+1 stress
        sentenceMainPGEnd22="[PG] Great. A last question. Should an abstract class be redefined in the child classes?";
        sentenceMainPGEnd23="[PG] Not at all !! "; //+1 stress
-       sentenceMainPGEnd24="[PG] Well, that's enough for the questions. I will look at your exercce later. You can go. Have a good day.";
-       sentenceMainPGEnd25="Classical end for a day in GPhy. This project is finish, but unfortunatly, you still have 5 other reports and 6 presentation for next week... May the odds be with you.";
+       sentenceMainPGEnd24="[PG] Well, that's enough for the questions. I will look at your exercise later. You can go. Have a good evening.";
+       sentenceMainPGEnd25="Classical end for a day in GPhy. This project is finished, but unfortunatly, you still have 5 other reports and 6 presentation for next week... May the odds be with you.";
        
        
        
@@ -374,48 +374,48 @@ public class Sentences
        listMainPGEnd.add(sentenceMainPGEnd25);
        
        //The conversation sentences for the player
-       sentenceMainPlayerToMrsGeniet1="[You] *whispering* Hello, I would like the Martin diploma please. He asked me to go get it.";
+       sentenceMainPlayerToMrsGeniet1="[You] *whispering* Hello, I would like Martin's diploma please. He asked me to go get it.";
        sentenceMainPlayerToMrsGeniet2="[You] *out loud* Hello, can I have Martin's diploma please?!";
-       sentenceMainPlayerToMrsGeniet3="[You] *whispering* I'm sorry. It's possible to have Martin's diploma please?";
+       sentenceMainPlayerToMrsGeniet3="[You] *whispering* I'm sorry. Is it possible to have Martin's diploma please?";
        sentenceMainPlayerToMrsGeniet4="[You] *yelling* I can't hear what you're saying, this L3 student is speaking too loudly!";
        
-       sentenceMainPlayerToAxel1="[You] Ouh? What? A work? Are you joking?";
+       sentenceMainPlayerToAxel1="[You] Eh? What? A project? Are you joking?";
        sentenceMainPlayerToAxel2="[You] No, I did not even look at the subject.";
        sentenceMainPlayerToAxel3="[You] Stop stressing me ! Have you already started, at least?";
        sentenceMainPlayerToAxel4="[You] Sh*t, I must quickly find my group !";
        sentenceMainPlayerToAxel5="[You] Well, you're as much in the sh*t as I am";
-       sentenceMainPlayerToAxel6="[You] And where is PG so?";
-       sentenceMainPlayerToAxel7="[You] Tt scares me to go see him :$";
+       sentenceMainPlayerToAxel6="[You] And where is PG?";
+       sentenceMainPlayerToAxel7="[You] It scares me to go see him...";
        
        sentenceMainPlayerToMartin1="[You] So-So, we have to begin the COOPOO subject. Are you available?";
        sentenceMainPlayerToMartin2="[You] Good, what are you doing?";
        sentenceMainPlayerToMartin3="[You] You lost it?";
-       sentenceMainPlayerToMartin4="[You] Okey so. I can go to find it. But where is you diploma?";
-       sentenceMainPlayerToMartin5="[You] I can go to find your diploma, but it is need free... Against a chocolate bread?";
-       sentenceMainPlayerToMartin6="[You] So, okey, I'm going ! Stay here !";
+       sentenceMainPlayerToMartin4="[You] Okay so, I can go find it. But where is it?";
+       sentenceMainPlayerToMartin5="[You] I can go find your diploma, but it is not free... Against a chocolate bread?";
+       sentenceMainPlayerToMartin6="[You] So, okay, I'm going ! Stay here !";
        sentenceMainPlayerToMartin7="[You] Luckily I'm kind, a kiss in return? :3";
        
        sentenceMainPlayerToNolan1="[You] Yes, who is it?";
        sentenceMainPlayerToNolan2="[You] *Say nothing, and try to escape the toilets on tiptoe*";
        sentenceMainPlayerToNolan3="[You] I'm so sorry, man ! Forgive me please !";
-       sentenceMainPlayerToNolan4="[You] *Make fun of a guy like him stuck in the toilet, laugh, and insult him*";
+       sentenceMainPlayerToNolan4="[You] *Make fun of a guy like him stuck in the toilets, laugh, and insult him*";
        sentenceMainPlayerToNolan5="[You] Why are you locked?";
-       sentenceMainPlayerToNolan6="[You] A simple solution is to call someone. For example PG. Right?";
+       sentenceMainPlayerToNolan6="[You] A simple solution is to call someone. For example PG, do I do that?";
        sentenceMainPlayerToNolan7="[You] Ahah, you're not smart !";
-       sentenceMainPlayerToNolan8="[You] Hum okey, I'll try to find a solution.";
+       sentenceMainPlayerToNolan8="[You] Hum okay, I'll try to find a solution.";
        sentenceMainPlayerToNolan9="[You] Oh, not cool for you..";
        sentenceMainPlayerToNolan10="[You] Okey, I'll try to smash that door !";
        
        sentenceMainPlayerToGuardian1="[You] Who are you?";
-       sentenceMainPlayerToGuardian2="[You] Yes... My friend is locked into the toilets and I need help to open it.";
-       sentenceMainPlayerToGuardian3="[You] Okey, so can you help me? Hum, my friend is locked and I want him out.";
+       sentenceMainPlayerToGuardian2="[You] Yes... My friend is locked into the toilets and I need help to open the door.";
+       sentenceMainPlayerToGuardian3="[You] Okay, so can you help me? Hum, my friend is locked and I want to get him out.";
        sentenceMainPlayerToGuardian4="[You] Hum.. You're weird.";
        sentenceMainPlayerToGuardian5="[You] How to use it?";
        sentenceMainPlayerToGuardian6="[You] Thanks.";
        
        sentenceMainPlayerToPGTD1="[You] Hello, I would like to have the COOPOO subject, please.";
        sentenceMainPlayerToPGTD2="[You] Can I have the COOPOO subject? I have to do it quickly";
-       sentenceMainPlayerToPGTD3="[You] Yes, but it is pressing !!!";
+       sentenceMainPlayerToPGTD3="[You] Yes, but it is urgent !!!";
        sentenceMainPlayerToPGTD4="[You] Yes, I am sorry...";
        sentenceMainPlayerToPGTD5="[You] Of course, I'll get started right now.";
        sentenceMainPlayerToPGTD6="[You] It will be easy, my mates are intelligent !";
@@ -424,12 +424,12 @@ public class Sentences
        sentenceMainPlayerToPGEnd2="[You] So.. Sorry.. *blushing*";
        sentenceMainPlayerToPGEnd3="[You] I found it in a cabinet in TP1.";
        sentenceMainPlayerToPGEnd4="[You] Your door was open! I just knocked, and I went in.";
-       sentenceMainPlayerToPGEnd5="[You] I knock the door, and as it was open, I entered.";
-       sentenceMainPlayerToPGEnd6="[You] It was Allan who opened me, seeing that I was looking for you";
+       sentenceMainPlayerToPGEnd5="[You] I knocked at the door, and as it was open, I entered.";
+       sentenceMainPlayerToPGEnd6="[You] It was Allan who opened it for me, seeing that I was looking for you";
        sentenceMainPlayerToPGEnd7="[You] *Decide to run away, quickly*";
        sentenceMainPlayerToPGEnd8="[You] *Stay here and wait for him to come back*";
        sentenceMainPlayerToPGEnd9="[You] I just wanted to give you the COOPOO report.";
-       sentenceMainPlayerToPGEnd10="[You] I do not know anymore...";
+       sentenceMainPlayerToPGEnd10="[You] I do not remember...";
        sentenceMainPlayerToPGEnd11="[You] *Give him the coopoo report.*";
        sentenceMainPlayerToPGEnd12="[You] *Hesitate, and tell him that you will modify the copy*";
        sentenceMainPlayerToPGEnd13="[You] *Escape as far as possible*";
@@ -438,7 +438,7 @@ public class Sentences
        sentenceMainPlayerToPGEnd16="[You] They had other things to do.";
        sentenceMainPlayerToPGEnd17="[You] Yes. In addition to give you the copy, I did everything myself. They did not even help me.";
        sentenceMainPlayerToPGEnd18="[You] It does not matter, they had exercises to finish.";
-       sentenceMainPlayerToPGEnd19="[You] Yes, an abstract class must be always instantiated.";
+       sentenceMainPlayerToPGEnd19="[You] Yes, an abstract class must always be instantiated.";
        sentenceMainPlayerToPGEnd20="[You] Hum, no.";
        sentenceMainPlayerToPGEnd21="[You] Aggregation is stronger";
        sentenceMainPlayerToPGEnd22="[You] Composition is stronger";
