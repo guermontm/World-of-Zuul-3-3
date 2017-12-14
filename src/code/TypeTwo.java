863af5793147bed399e9f0fdd9cd03c57a80a632
NolanLacroix <33682165+NolanLacroix@users.noreply.github.com>
2017-12-14 22:21:47 +0100

    Update TypeTwo.java
    add comm

--- src/code/TypeTwo.java
+++ src/code/TypeTwo.java
@@ -4,7 +4,10 @@ package code;
 /**
  * This class inherits from player and represents a player of the second type
  * It means that the attributes are initialised with specific values
- *
+ * This player is named Elie and owns a special power : "furtif". This power can be activated or disabled.
+ * If "furtif" is on, the player cannot encounter random events while moving from room to room.
+ * 
+ * 
  * @author (Group 5)
  * @version (09/11/17)
  */
@@ -14,7 +17,8 @@ public class TypeTwo extends Player
     
     /**
      * Constructor for objects of class TypeTwo
-     * This method gives the specific values of the different attributes
+     * This method gives the specific values of the different attributes.
+     * The player hasalso a "furtif" boolean.
      */
     public TypeTwo()
     {
@@ -27,6 +31,11 @@ public class TypeTwo extends Player
         setStress(5);
     }
     
+    /**
+     * Method getFurtif allowing to return the boolean furtif
+     *
+     * @return the boolean furtif
+     */
     public boolean getFurtif()
     {
         return furtif;    
